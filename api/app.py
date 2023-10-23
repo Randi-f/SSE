1476418c365e8d36d1d204ca00233771008605e3
Shihan Fu <shihan.fu23@imperial.ac.uk>
2023-10-23 15:16:49 +0100

    1023 q2

--- api/app.py
+++ api/app.py
@@ -1,18 +1,19 @@
-from flask import Flask,render_template,request
-app = Flask(__name__,static_url_path='/SSE-LAB2_static', static_folder='./static')
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_url_path="/SSE-LAB2_static", static_folder="./static")
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
     # return "Hello , my new app!"
 
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
-    if(input_age<'18'):
+    if input_age < "18":
         return render_template("hello.html", name=input_name, age=input_age)
     else:
         return render_template("helloAdult.html", name=input_name, age=input_age)
-    
-    
